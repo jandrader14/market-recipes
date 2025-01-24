0d340db53f8416b2a4721ae943068cbdc6ee53f8
Johanna Andrade <andrader.johanna@gmail.com>
2025-01-23 21:20:17 -0500

    Cargué los archivos de la primera versión de mi codigo

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+  
+    loginForm.addEventListener('submit', (event) => {
+      event.preventDefault(); // Evita el envío del formulario
+  
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      // Credenciales por defecto
+      const defaultEmail = 'usuario@ejemplo.com';
+      const defaultPassword = 'Ejemplo123#';
+  
+      // Verifica las credenciales
+      if (email === defaultEmail && password === defaultPassword) {
+        // Redirige a otra página
+        window.location.href = 'dashboard.html';
+      } else {
+        alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+      }
+    });
+  });
